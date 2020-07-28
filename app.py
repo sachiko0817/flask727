73a2b49f5341675d2d3fc2253321b760ea4dfbf3
Sachiko Harada <gooddays_0817@yahoo.co.jp>
2020-07-28 21:02:55 +0900

    テンプレートまでやったよ！

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,5 +9,24 @@ def hello():
 def top():
         return "ここはトップだよ〜"
 
+@app.route('/hello/<text>')
+def namehello(text):
+        return text + "さんこんにちは"
+
+@app.route('/index')
+def index():
+        name="sunabaco"
+        age = 20
+        address = "田町"
+        return render_template('index.html',user_name = name,user_address = address, user_age = age) 
+
+@app.route('/weather')
+def weather():
+    py_weather = "晴れ"
+    return render_template('weather.html',html_weather = py_weather)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
