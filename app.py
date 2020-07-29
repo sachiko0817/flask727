ca56a9fd141523e7736fbd9b0f01e43aa34f1907
Sachiko Harada <gooddays_0817@yahoo.co.jp>
2020-07-29 19:42:55 +0900

    addのgetまで実装しました

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
@@ -24,6 +25,32 @@ def index():
 def weather():
     py_weather = "晴れ"
     return render_template('weather.html',html_weather = py_weather)
+
+@app.route('/dbtest')
+def dbtest():
+    conn = sqlite3.connect('flasktest.db')
+    c = conn.cursor()
+    c.execute("SELECT name,age,address FROM user WHERE id = 1")
+    user_info = c. fetchone()
+    c.close()
+    print(user_info)
+    return render_template('dbtest.html',db_userinfo = user_info)
+
+@app.route('/add')
+def add_get():
+    return render_template('add.html')
+
+@app.route('/add',methods=['post'])
+de app_post():
+    py_task = request.form.get("task")
+    conn = sqlite3.connect('flasktest.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO task VALUES (null,?)",(py_task,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+
 
 
 if __name__ == '__main__':
